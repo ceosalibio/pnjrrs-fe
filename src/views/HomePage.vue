153282f8a99c312f8f123bb23272b6d79c34df65
Arceo Jr. Salibio <salibioarceo@gmail.com>
2025-09-19 19:39:25 +0800

    boilerplate

---
+++ src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Count: {{ counter.count }}</p>
+    <button @click="counter.increment">Increment</button>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+const counter = useCounterStore()
+</script>
